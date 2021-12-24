ba93d760582743c84960d1ac39f8e3bde314096a
Nicholas Currault <nicholas.curr@gmail.com>
2021-12-23 23:00:10 -0600

    finished transition code

--- 2021/23/23-1.py
+++ 2021/23/23-1.py
@@ -31,19 +31,30 @@ def is_occupied(state, loc):
     return state[loc] in "ABCD"
 
 LEFT_PATHS = {
-    2: [1, 0]
-    5: [4, None, 1, 0]
-    8: [7, None, 4, None, 1, 0]
-    11: [10, None, 7, None, 4, None, 1, 0]
+    2: [1, 0],
+    5: [4, None, 1, 0],
+    8: [7, None, 4, None, 1, 0],
+    11: [10, None, 7, None, 4, None, 1, 0],
 }
 RIGHT_PATHS = {
-    2: [4, None, 7, None, 10, None, 13, 14]
-    5: [7, None, 10, None, 13, 14]
-    8: [10, None, 13, 14]
-    11: [13, 14]
+    2: [4, None, 7, None, 10, None, 13, 14],
+    5: [7, None, 10, None, 13, 14],
+    8: [10, None, 13, 14],
+    11: [13, 14],
 }
 PER_MOVE_ENERGIES = {"A": 1, "B": 10, "C": 100, "D": 1000}
 ROOMS = [(3, 2), (6, 5), (9, 8), (12, 11)]
+ROOM_BOTTOMS = [x[0] for x in ROOMS]
+
+print_state_str_format = """
+#############
+#{0}{1}.{4}.{7}.{10}.{13}{14}#
+###{2}#{5}#{8}#{11}###
+  #{3}#{6}#{9}#{12}#
+  #########
+"""
+def state_str(state):
+    return print_state_str_format.format(*state)
 
 def swap_chars(s, i, j):
     l = list(s)
@@ -51,12 +62,65 @@ def swap_chars(s, i, j):
     return "".join(l)
 
 def room_destinations_from_hallway(state, traveler):
-    for bottom, top in rooms:
+    for bottom, top in ROOMS:
         if is_occupied(state, top):
             continue
         elif is_occupied(state, bottom) and state[bottom] != traveler:
             continue
         yield top if is_occupied(state, bottom) else bottom
+
+def hallway_to_room_dist(state, hallway_loc, destination):
+    plus_one = destination in ROOM_BOTTOMS
+    if plus_one:
+        destination -= 1
+    # TODO just subset of [1,4,7,10,13] in open interval?
+    blockers = {
+        (0, 2): [1],
+        (0, 5): [1, 4],
+        (0, 8): [1, 4, 7],
+        (0, 11): [1, 4, 7, 10],
+        (1, 5): [4],
+        (1, 8): [4, 7],
+        (1, 11): [4, 7, 10],
+        (4, 8): [7],
+        (4, 11): [10],
+        (7, 2): [4],
+        (7, 11): [10],
+        (10, 2): [4, 7],
+        (10, 5): [7],
+        (13, 2): [4, 7, 10],
+        (13, 5): [7, 10],
+        (13, 8): [10],
+        (14, 2): [4, 7, 10, 13],
+        (14, 5): [7, 10, 13],
+        (14, 8): [10, 13],
+        (14, 11): [13],
+    }.get((hallway_loc, destination), [])
+    for b in blockers:
+        if is_occupied(state, b):
+            return
+    return {
+        (0, 2): 3,
+        (0, 5): 5,
+        (0, 8): 7,
+        (0, 11): 9,
+        (1, 5): 4,
+        (1, 8): 6,
+        (1, 11): 8,
+        (4, 8): 4,
+        (4, 11): 6,
+        (7, 2): 4,
+        (7, 11): 4,
+        (10, 2): 6,
+        (10, 5): 4,
+        (13, 2): 8,
+        (13, 5): 6,
+        (13, 8): 4,
+        (14, 2): 9,
+        (14, 5): 7,
+        (14, 8): 5,
+        (14, 11): 3
+    }.get((hallway_loc, destination), 2)
 
 def valid_transitions(state):
     for loc in range(15):
@@ -65,12 +129,16 @@ def valid_transitions(state):
         occupant = state[loc]
         per_move_energy = PER_MOVE_ENERGIES[occupant]
         if is_hallway(loc):
-            pass  # must move into a room
+            for destination in room_destinations_from_hallway(state, occupant):
+                d = hallway_to_room_dist(state, loc, destination)
+                if d is None:
+                    continue
+                yield swap_chars(state, loc, destination), d * per_move_energy
         else:
             bottoms = [room[0] for room in ROOMS]
             current_loc = loc
             if loc in bottoms:
-                if is_occupied(statte, loc - 1):
+                if is_occupied(state, loc - 1):
                     continue # blocked in
                 else:
                     current_loc -= 1
