7f2edcec09a9193a4c3f235724c6d7aff2b7456f
Nicholas Currault <nicholas.curr@gmail.com>
2022-12-05 21:05:39 -0800

    day 6

--- 2022/6/6-2.py
+++ 2022/6/6-2.py
@@ -5,11 +5,10 @@ PRACTICE = False
 with open("test.txt" if PRACTICE else "input", "r") as f:
     file_content = f.read().strip()
 
+MARKER_LEN = 14
 
-last_four = ""
-
-for i in range(4, len(file_content) + 1):
-    last_four = file_content[i - 4 : i]
-    if len(set(last_four)) == 4:
+for i in range(MARKER_LEN, len(file_content) + 1):
+    last_four = file_content[i - MARKER_LEN : i]
+    if len(set(last_four)) == MARKER_LEN:
         print(i)
         break
