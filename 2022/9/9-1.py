e18bc600793a417436abb00a836cc7bec85c672c
Nicholas Currault <nicholas.curr@gmail.com>
2022-12-09 00:28:43 -0800

    day 9 with a bug

--- 2022/9/9-1.py
+++ 2022/9/9-1.py
@@ -8,3 +8,48 @@ with open("test.txt" if PRACTICE else "input", "r") as f:
     file_content = f.read().strip()
 
 lines = file_content.split("\n")
+
+head_position = [0, 0]
+tail_position = [0, 0]
+
+
+def move(direction):
+    match direction:
+        case "U":
+            head_position[1] += 1
+        case "D":
+            head_position[1] -= 1
+        case "R":
+            head_position[0] += 1
+        case "L":
+            head_position[0] -= 1
+        case _:
+            print(direction)
+
+    x_sign = 0
+    y_sign = 0
+    if abs(head_position[0] - tail_position[0]) > 1 or abs(head_position[1] - tail_position[1]) > 1:
+        if head_position[0] > tail_position[0]:
+            x_sign = 1
+        elif head_position[0] < tail_position[0]:
+            x_sign = -1
+
+        if head_position[1] > tail_position[1]:
+            y_sign = 1
+        elif head_position[1] < tail_position[1]:
+            y_sign = -1
+
+    tail_position[0] += x_sign
+    tail_position[1] += y_sign
+
+
+tail_positions = {(0,0)}
+
+for line in lines:
+    direction, num_moves = line.split()
+    for _ in range(int(num_moves)):
+        tail_positions.add(tuple(tail_position))
+        # print(tuple(tail_position))
+        move(direction)
+
+print(len(tail_positions))
