d568c162a2cf7e8da08f70837f7b9f89e84a06e4
Nicholas Currault <nicholas.curr@gmail.com>
2022-12-09 00:54:20 -0800

    bugfix

--- 2022/9/9-1.py
+++ 2022/9/9-1.py
@@ -3,7 +3,7 @@ import re
 import math
 from collections import defaultdict
 
-PRACTICE = True
+PRACTICE = False
 with open("test.txt" if PRACTICE else "input", "r") as f:
     file_content = f.read().strip()
 
@@ -24,7 +24,7 @@ def move(direction):
         case "L":
             head_position[0] -= 1
         case _:
-            print(direction)
+            print(direction, "invalid")
 
     x_sign = 0
     y_sign = 0
@@ -42,14 +42,16 @@ def move(direction):
     tail_position[0] += x_sign
     tail_position[1] += y_sign
 
+    assert abs(head_position[0] - tail_position[0]) <= 1
+    assert abs(head_position[1] - tail_position[1]) <= 1
 
-tail_positions = {(0,0)}
+
+tail_positions = {(0, 0)}
 
 for line in lines:
     direction, num_moves = line.split()
     for _ in range(int(num_moves)):
-        tail_positions.add(tuple(tail_position))
-        # print(tuple(tail_position))
         move(direction)
+        tail_positions.add(tuple(tail_position))
 
 print(len(tail_positions))
