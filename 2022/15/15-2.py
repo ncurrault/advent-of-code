1320ddc393bfc88f86f9cef5d4c1d189b64ca3a2
Nicholas Currault <nicholas.curr@gmail.com>
2022-12-15 02:18:16 -0800

    day 15

--- 2022/15/15-2.py
+++ 2022/15/15-2.py
@@ -3,12 +3,14 @@ import re
 import math
 from collections import defaultdict
 from enum import Enum
+import itertools
+import tqdm
 
 PRACTICE = False
 with open("test.txt" if PRACTICE else "input", "r") as f:
     file_content = f.read().strip()
 
-TARGET_Y = 10 if PRACTICE else 2000000
+MAX_COORD = 20 if PRACTICE else 4000000
 
 
 class Intervals:
@@ -29,20 +31,20 @@ class Intervals:
                 return True
         return False
 
-    def total_size(self, exclude):
-        res = set()
-        for start, end in self.sub_intervals:
-            res.update(range(start, end + 1))
-        res.difference_update(exclude)
-        return len(res)
+    # TODO this function is buggy (returns some inaccurate points)
+    # but it narrows the search space enough
+    def missing_points(self):
+        sorted_intervals = sorted(self.sub_intervals, key=lambda x: x[0])
+        for i in range(len(sorted_intervals) - 1):
+            if sorted_intervals[i][1] + 1 < sorted_intervals[i + 1][0]:
+                yield sorted_intervals[i][1] + 1
 
 
 def manhattan_distance(square_a, square_b):
     return abs(square_a[0] - square_b[0]) + abs(square_a[1] - square_b[1])
 
 
-ruled_out = Intervals()
-beacons_in_target_line = set()
+beacons = []
 
 for line in file_content.split("\n"):
     parsed = re.findall(
@@ -53,15 +55,22 @@ for line in file_content.split("\n"):
     assert len(parsed[0]) == 4
     sensor_x, sensor_y, beacon_x, beacon_y = [int(n) for n in parsed[0]]
 
-    if beacon_y == TARGET_Y:
-        beacons_in_target_line.add(beacon_x)
-
     dist = manhattan_distance((sensor_x, sensor_y), (beacon_x, beacon_y))
+    beacons.append((sensor_x, sensor_y, dist))
 
-    dist_to_center = abs(sensor_y - TARGET_Y)
-    if dist_to_center > dist:
-        continue  # can't rule out anything here
-    remaining_dist = dist - dist_to_center
-    ruled_out.union(sensor_x - remaining_dist, sensor_x + remaining_dist)
 
-print(ruled_out.total_size(beacons_in_target_line))
+for y in tqdm.trange(MAX_COORD):
+    ruled_out = Intervals()
+    for sensor_x, sensor_y, dist in beacons:
+        dist_to_center = abs(sensor_y - y)
+        if dist_to_center > dist:
+            continue  # can't rule out anything here
+        remaining_dist = dist - dist_to_center
+        ruled_out.union(sensor_x - remaining_dist, sensor_x + remaining_dist)
+    for x in ruled_out.missing_points():
+        if any(
+            manhattan_distance((x, y), (sensor_x, sensor_y)) <= dist
+            for sensor_x, sensor_y, dist in beacons
+        ):
+            continue
+        print(x, y, x * 4000000 + y)
