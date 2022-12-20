0aa50fccce8edf15f3f432723b30e3d78fdd5773
Nicholas Currault <nicholas.curr@gmail.com>
2022-12-19 23:53:48 -0600

    day 20

--- 2022/20/20-2.py
+++ 2022/20/20-2.py
@@ -13,19 +13,20 @@ with open("test.txt" if PRACTICE else "input", "r") as f:
     file_content = f.read().strip()
 lines = file_content.split("\n")
 
-labelled_numbers = [(i, int(l)) for i, l in enumerate(lines)]
+labelled_numbers = [(i, 811589153 * int(l)) for i, l in enumerate(lines)]
 file_size = len(labelled_numbers)
 
-for id_to_move in range(file_size):
-    old_idx = None
-    for i, (k, n) in enumerate(labelled_numbers):
-        if k == id_to_move:
-            old_idx = i
-            break
-    assert old_idx is not None
-    new_idx = (old_idx + n) % (file_size - 1)
-    labelled_numbers.pop(old_idx)
-    labelled_numbers.insert(new_idx, (k, n))
+for _ in range(10):
+    for id_to_move in range(file_size):
+        old_idx = None
+        for i, (k, n) in enumerate(labelled_numbers):
+            if k == id_to_move:
+                old_idx = i
+                break
+        assert old_idx is not None
+        new_idx = (old_idx + n) % (file_size - 1)
+        labelled_numbers.pop(old_idx)
+        labelled_numbers.insert(new_idx, (k, n))
 
 final_numbers = [n for _, n in labelled_numbers]
 zero_idx = final_numbers.index(0)
